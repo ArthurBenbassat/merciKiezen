6a454c1982fcd773ebffb7338029678bf2b7f049
Arthur Benbassat <arthur.benbassat@outlook.com>
2019-06-03 14:03:35 +0200

    qsdf

--- merciKiezen/Form1.Designer.vb
+++ merciKiezen/Form1.Designer.vb
@@ -22,10 +22,28 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(60, 43)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents ListBox1 As ListBox
 End Class
--- merciKiezen/Form1.vb
+++ merciKiezen/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+
+        Dim chocolade As String() = {"melk", "koffie-creme", "hazelnoot", "marsepein", "zwarte-creme", "zwarte-mouse", "praline-creme", "hazelnoot-amandel"}
+        Dim persoon As String() = {"Arthur", "Kyle", "Tim", "Denis"}
+        Dim getal As Integer = Int(Rnd() * 8) + 1
+        Dim getal2 As Integer = Int(Rnd() * 4) + 1
+        ListBox1.Items.Add(persoon(getal2) & " krijgt " & chocolade(getal))
+    End Sub
 End Class
